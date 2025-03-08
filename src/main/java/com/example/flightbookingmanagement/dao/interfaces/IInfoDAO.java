ca733c36d42b12c65c679c4dc0fbe2ae3029066e
CHUHAI <haichu7793@gmail.com>
2025-03-08 17:06:06 +0700

    delete redundant files

--- src/main/java/com/example/flightbookingmanagement/dao/interfaces/IInfoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.flightbookingmanagement.dao.interfaces;
-
-import com.example.flightbookingmanagement.dto.InfoDTO;
-
-public interface IInfoDAO {
-    InfoDTO getUserInfo(int userId);
-    boolean updateUserInfo(InfoDTO userInfo);
-}
