e23f57e06d6ca68238480ab1e8d059bf8e3ae118
CHUHAI <haichu7793@gmail.com>
2025-03-13 14:12:59 +0700

    Clean SQLconstants of BookingService

--- src/main/java/com/example/flightbookingmanagement/service/BookingService.java
+++ src/main/java/com/example/flightbookingmanagement/service/BookingService.java
@@ -20,37 +20,24 @@ import java.util.*;
 import static com.example.flightbookingmanagement.config.DatabaseConfig.getConnection;
 
 public class BookingService {
-
-    public static final String SELECT_FLIGHT_ID_TRAVEL_DATE = "SELECT f.flight_id, DATE(f.departure_time) as departure_date\n" +
-            "FROM flights f\n" +
-            "WHERE flight_code = ?;";
-    public static final String INSERT_PAYMENT="INSERT INTO payments (ticket_id, amount, payment_status)\n " +
-                                                "VALUES (?, ?, ?)";
-
-    public static final String INSERT_TICKET ="INSERT INTO tickets (user_id, flight_id, travel_date, seat_number, status)\n " +
-            "VALUES (?, ?, ?, ?, 'booked')";
-
     public void bookTicket(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String flight_code = request.getParameter("flightCode");
         int user_id = Integer.parseInt(request.getParameter("userId"));
         int seatNumber = Integer.parseInt(request.getParameter("seat_number"));
-        System.out.println(flight_code);
-        System.out.println(user_id);
-        System.out.println(seatNumber);
-        int price = 2000000; //  giá vé duoc tinh tu bang flight va luggage
+        String luggage_weight = request.getParameter("luggage");
 
-        Connection conn = null;
+        Connection connection = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         int rowsInserted = -1;
         int flightId = -1;
         try {
-            conn = DatabaseConfig.getConnection();
-            conn.setAutoCommit(false); // Tắt tự động commit
+            connection = DatabaseConfig.getConnection();
+            connection.setAutoCommit(false); // Tắt tự động commit
 
             // 1. Lấy flight_id và departure_date tương ứng flight_code
-            stmt = conn.prepareStatement(SELECT_FLIGHT_ID_TRAVEL_DATE);
+            stmt = connection.prepareStatement(SQLConstants.SELECT_FLIGHT_ID_TRAVEL_DATE);
             stmt.setString(1, flight_code);
             rs = stmt.executeQuery();
 
@@ -58,39 +45,57 @@ public class BookingService {
                 flightId = rs.getInt("flight_id");
                 Date travelDate = rs.getDate("departure_date");
 
-                // 1️⃣ Kiểm tra ghế trống
-                stmt = conn.prepareStatement("SELECT available_seats FROM flights WHERE flight_id = ? FOR UPDATE");
+                // 2. Kiểm tra ghế trống
+                stmt = connection.prepareStatement(SQLConstants.SELECT_AVAILABLE_SEATS);
                 stmt.setInt(1, flightId);
                 rs = stmt.executeQuery();
 
                 if (rs.next() && rs.getInt("available_seats") > 0) {
-                    // 2. Trừ số ghế trống
-                    stmt = conn.prepareStatement("UPDATE flights SET available_seats = available_seats - 1 WHERE flight_id = ?");
+                    // 3. Trừ số ghế trống
+                    stmt = connection.prepareStatement(SQLConstants.UPDATE_AVAILABLE_SEATS_AFTER_BOOKING);
                     stmt.setInt(1, flightId);
                     stmt.executeUpdate();
 
-                    // 3. Thêm vé vào bảng tickets
-                    stmt = conn.prepareStatement(INSERT_TICKET,Statement.RETURN_GENERATED_KEYS);
+                    
+                    // 5. Thêm vé vào bảng tickets
+                    stmt = connection.prepareStatement(SQLConstants.INSERT_TICKET,Statement.RETURN_GENERATED_KEYS);
                     stmt.setInt(1, user_id);
                     stmt.setInt(2, flightId);
                     stmt.setDate(3, travelDate);
                     stmt.setInt(4, seatNumber);
                     rowsInserted = stmt.executeUpdate();
 
+
 //                Lấy ID (khóa chính tự động tăng) của bản ghi vừa được chèn vào cơ sở dữ liệu.
                     rs = stmt.getGeneratedKeys();
                     int ticketId = 0;
                     if (rs.next()) {
                         ticketId = rs.getInt(1);
-                        // 4. Thêm giao dịch thanh toán
-                        stmt = conn.prepareStatement(INSERT_PAYMENT);
-                        stmt.setInt(1, ticketId);
-                        stmt.setInt(2, price);
-                        stmt.setString(3, "pending");
+
+
+                        // Thêm mới luggage
+                        stmt = connection.prepareStatement(SQLConstants.INSERT_LUGGAGE);
+                        stmt.setInt(1,ticketId);
+                        stmt.setString(2,luggage_weight);
                         stmt.executeUpdate();
-                        // 5. Xác nhận giao dịch
-                        conn.commit();
-                        response.getWriter().println("BOOKING SUCCESSFULLY!");
+
+
+                        // Lấy tổng tiền cần thanh toán được tính dựa trên từ bảng flights và luggage
+                        stmt = connection.prepareStatement(SQLConstants.GET_TOTAL_PRICE);
+                        stmt.setInt(1,ticketId );
+                        rs = stmt.executeQuery();
+                        if (rs.next()) {
+                            int total_price = rs.getInt("total_price");
+                            // 6. Thêm giao dịch thanh toán
+                            stmt = connection.prepareStatement(SQLConstants.INSERT_PAYMENT);
+                            stmt.setInt(1, ticketId);
+                            stmt.setInt(2, total_price);
+                            stmt.setString(3, "pending");
+                            stmt.executeUpdate();
+                            // 5. Xác nhận giao dịch
+                            connection.commit();
+                            response.getWriter().println("BOOKING SUCCESSFULLY!");
+                        }
                     } else {
                         response.getWriter().println("OUT OF SEAT!");
                     }
@@ -99,7 +104,7 @@ public class BookingService {
 
         } catch (Exception e) {
             try {
-                if (conn != null) conn.rollback(); // Hủy giao dịch nếu lỗi
+                if (connection != null) connection.rollback(); // Hủy giao dịch nếu lỗi
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
@@ -108,7 +113,7 @@ public class BookingService {
             try {
                 if (rs != null) rs.close();
                 if (stmt != null) stmt.close();
-                if (conn != null) conn.setAutoCommit(true); // Bật lại AutoCommit
+                if (connection != null) connection.setAutoCommit(true); // Bật lại AutoCommit
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -134,10 +139,7 @@ public class BookingService {
 
         // Kết nối đến database để lấy danh sách ghế
         try (Connection conn = DatabaseConfig.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT t.seat_number, t.status \n" +
-                                                             "FROM tickets t\n" +
-                                                             "JOIN flights f on t.flight_id = f.flight_id\n" +
-                                                             "WHERE flight_code = ?;")) {
+             PreparedStatement ps = conn.prepareStatement(SQLConstants.SELECT_SEAT_NUM_STATUS_BY_FLIGHT_CODE)) {
 
             ps.setString(1,flight_code); // Lấy mã chuyến bay từ request
             ResultSet rs = ps.executeQuery();
@@ -164,3 +166,4 @@ public class BookingService {
         session.setAttribute("chosenSearchedTicket", chosenSearchedTicket);
     }
 }
+
